9ada0eb8733c0f8fd3200f34fa73b4cbb7da178e
Colin Graber <cgraber@purdue.edu>
2013-07-02 00:06:49 -0400

    Began dashing implementation

--- src/entities/player.py
+++ src/entities/player.py
@@ -13,15 +13,22 @@ class Player(Object):
         self.g = grid
         super().__init__(x,y)
         self.grav = 25
-        self.acc = 2
+        self.walk_acc = 2
+        self.dash_acc = 8
+        self.aerial_acc = 2
         self.jump = 200
         self.jumpdecay = 0.85
         self._jump = 0 
         self._jumpdecay = 0.85
         self._isWalking = 0
         self.friction = 0
-        self.v_x_max = 5
+        self.air_friction = 0
+        self.walk = 15
+        self.dash = 15
+        self.dashing = False
         self.v_y_max = 300
+        self.v_x_max = 0
+        self.v_x_maxair = self.walk
         self.v_x = 0
         self.v_y = 0
         self.jumpno = 0
@@ -29,6 +36,7 @@ class Player(Object):
         self.prev_up = False
         self.prev_left = False
         self.prev_right = False
+        self.tap_time = 0
     
     def event_create(self):
         self.setSprite(Sprite(self.assets.images["boy"]["boy_idle"],32,64),-16,-32)
@@ -37,23 +45,50 @@ class Player(Object):
     def update_keys(self):
         self.prev_up = self.game.keys[K_w]
         self.prev_left = self.game.keys[K_a]
-        self.prev_right = self.game.keys[K_d]
+        #ensures game won't register both left and right at same time
+        if self.prev_left == True:
+            self.prev_right = False
+        else:
+            self.prev_right = self.game.keys[K_d]
+        
+    def changed_direction(self):
+        if self.game.keys[K_a] and self.prev_right == True:
+            return True
+        elif self.game.keys[K_d] and self.prev_left == True:
+            return True
+        else:
+            return False
+    
+    def pressed_left(self):
+        return self.game.keys[K_a] and not self.prev_left
+    
+    def pressed_right(self):
+        return self.game.keys[K_d] and not self.prev_right   
+    
+    def update_dashing(self):
+        timeBetweenTaps = 500
+        if self.pressed_left() or self.pressed_right():
+            timeBetweenTaps = time.get_ticks() - self.tap_time
+            self.tap_time = time.get_ticks()
+        if self.aerial and not self.dashing:
+            self.dashing = False
+        elif self.dashing and (self.game.keys[K_a] or self.game.keys[K_d]):
+            #Maintain dash
+            self.dashing = True
+        elif not (self.game.keys[K_a] or self.game.keys[K_d]):
+            #Dashing stops if not moving left or right
+            self.dashing = False
+        elif timeBetweenTaps < 200:
+            self.dashing = True
+            print ("new dash")
+        else:
+            self.dashing = False
            
     def update(self):
-        
+        self.update_dashing()
         #Keys
         #Movement varies based on whether the player is aerial or not
         if self.aerial:
-            if self.game.keys[K_a]:
-                if self.v_x >= -self.v_x_max:
-                    self.v_x -= self.acc
-                else:
-                    self.v_x = -self.v_x_max
-            if self.game.keys[K_d]:
-                if self.v_x <= self.v_x_max:
-                    self.v_x += self.acc
-                else:
-                    self.v_x = self.v_x_max
             if  self.game.keys[K_w]:
                 if self.jumpno < 2:
                     if not self.prev_up:
@@ -61,23 +96,42 @@ class Player(Object):
                         self._jumpdecay = self.jumpdecay
                         self.v_y = 0
                         self.jumpno += 1
-                    
+                        if self.changed_direction():
+                            self.v_x = 0
                 self.v_y -= self._jump
                 self._jump *= self._jumpdecay
                 self._jumpdecay *= self._jumpdecay
+            if self.game.keys[K_a]:
+                if self.v_x > -self.v_x_max:
+                    self.v_x -= self.aerial_acc
+                else:
+                    self.v_x = -self.v_x_max
+            elif self.game.keys[K_d]:
+                if self.v_x < self.v_x_max:
+                    self.v_x += self.aerial_acc
+                else:
+                    self.v_x = self.v_x_max
+            
                 
         #On the ground
         else:
+            if self.dashing:
+                max_speed = self.dash
+                acc = self.dash_acc
+            else:
+                max_speed = self.walk
+                acc = self.walk_acc
+                
             if self.game.keys[K_a]:
-                if self.v_x >= -self.v_x_max:
-                    self.v_x -= self.acc
-                else:
-                    self.v_x = -self.v_x_max
+                    if self.v_x >= -max_speed:
+                        self.v_x -= acc
+                    else:
+                        self.v_x = -max_speed
             if self.game.keys[K_d]:
-                if self.v_x <= self.v_x_max:
-                    self.v_x += self.acc
+                if self.v_x <= max_speed:
+                    self.v_x += acc
                 else:
-                    self.v_x = self.v_x_max
+                    self.v_x = max_speed
             if  self.game.keys[K_w]:
                 self.aerial = True
                 self._jump = self.jump
@@ -86,7 +140,10 @@ class Player(Object):
                 self.v_y -= self._jump
                 self._jump *= self._jumpdecay
                 self._jumpdecay *= self._jumpdecay
-        
+                if self.dashing:
+                    self.v_x_max = math.fabs(self.v_x)
+                else:
+                    self.v_x_max = self.v_x_maxair
         if self.v_x != 0:
             if self._isWalking == 0:
                 self.setSprite(Sprite(self.assets.images["boy"]["boy_walk"],32,64))
@@ -101,7 +158,7 @@ class Player(Object):
             self._isWalking = 0
             self.assets.stopSound("walking_grass.ogg")
             self.setSprite(Sprite(self.assets.images["boy"]["boy_idle"],32,64))
-        
+
         #Physics
         clock = time.Clock()
         fps = clock.get_fps()
@@ -112,10 +169,14 @@ class Player(Object):
         self.v_y += self.grav
         if self.v_y > self.v_y_max: 
             self.v_y = self.v_y_max #Terminal velocity
-        self.v_x = self.v_x * (1-self.friction)
+        if self.aerial:
+            self.v_x = self.v_x * (1-self.air_friction)
+            print("Air friction = %f"%self.air_friction)
+        else:
+            self.v_x = self.v_x * (1-self.friction)
+            print("Ground friction = %f"%self.friction)
         if math.fabs(self.v_x) < 0.3:
             self.v_x = 0
-        
         # Hazard top/below
         if(self.v_y > 0):
             mod = 32
@@ -155,9 +216,8 @@ class Player(Object):
                 
         #nothing above or below  
         else:
-            self.friction = 0
+            #self.friction = 0
             self.aerial = True
-        
         # Hazard side
         if(self.v_x > 0):
             mod = 16
@@ -185,7 +245,7 @@ class Player(Object):
             x,y = self.g.getPosition(x,y)
             self.setX(x-mod)
             self.v_x = 0
-            
+           
         elif hstep:
             #self._jump == self.jump when grounded
             if self._jump == self.jump and ((self.game.keys[K_a] and self.v_x < 0) or (self.game.keys[K_d] and self.v_x > 0)):
@@ -199,6 +259,7 @@ class Player(Object):
                 self.setX(x-mod) 
                 self.v_x = 0
         
+        print("Moving %d"%self.v_x)
         self.move(self.v_x, newy)
         self.update_keys()
         
